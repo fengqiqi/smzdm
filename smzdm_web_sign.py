9004af3712515b03e9c0584f4e0c4151d4909295
fengqilove520 <1309012576@qq.com>
2021-11-09 16:42:46 +0800

    优化代码

--- smzdm_web_sign.py
+++ smzdm_web_sign.py
@@ -27,31 +27,31 @@ class SMZDM_Bot(object):
             'Sec-Fetch-Site': 'same-site',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
         }
-
+    """
+    对请求 盖乐世社区 返回的数据进行进行检查
+    1.判断是否 json 形式
+    """
     def __json_check(self, msg):
-        """
-        对请求 盖乐世社区 返回的数据进行进行检查
-        1.判断是否 json 形式
-        """
+
         try:
-            msg.json()
+            result=msg.json()
             return True
         except Exception as e:
             print(f'Error : {e}')
             return False
 
+    """
+    起一个什么值得买的，带cookie的session
+    cookie 为浏览器复制来的字符串
+    :param cookie: 登录过的社区网站 cookie
+    """
     def load_cookie_str(self, cookies):
-        """
-        起一个什么值得买的，带cookie的session
-        cookie 为浏览器复制来的字符串
-        :param cookie: 登录过的社区网站 cookie
-        """
         self.session.headers['Cookie'] = cookies
 
+    """
+    签到函数
+    """
     def checkin(self):
-        """
-        签到函数
-        """
         url = 'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin'
         msg = self.session.get(url)
         if self.__json_check(msg):
@@ -62,11 +62,11 @@ if __name__ == '__main__':
     sb = SMZDM_Bot()
     with open("./smzdm_cookie.json", "r") as json_file:
         data = json.load(json_file)
-	# 换成自己的cookie
     cookies = data['cookies']
     sb.load_cookie_str(cookies)
     res = sb.checkin()
     if(res['error_code'] != '0'):
-        print(res['error_msg']+"请确认cookie是否正确！")
+        print(res['error_msg'])
     else:
         print('签到执行完毕')
+    print('返回信息:', res)
