d4b0966756fbb1ec51b1791bac08a7bfd806e942
fengqilove520 <1309012576@qq.com>
2021-11-09 14:30:47 +0800

    什么值得买web端签到

---
+++ smzdm_web_sign.py
@@ -0,0 +1,63 @@
+"""
+什么值得买自动签到脚本
+使用github actions 定时执行
+@author : stark
+"""
+import requests,os
+
+class SMZDM_Bot(object):
+    def __init__(self):
+        self.session = requests.Session()
+        # 添加 headers
+        self.session.headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Host': 'zhiyou.smzdm.com',
+            'Referer': 'https://www.smzdm.com/',
+            'Sec-Fetch-Dest': 'script',
+            'Sec-Fetch-Mode': 'no-cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+        }
+
+    def __json_check(self, msg):
+        """
+        对请求 盖乐世社区 返回的数据进行进行检查
+        1.判断是否 json 形式
+        """
+        try:
+            result = msg.json()
+            print(result)
+            return True
+        except Exception as e:
+            print(f'Error : {e}')
+            return False
+
+    def load_cookie_str(self, cookies):
+        """
+        起一个什么值得买的，带cookie的session
+        cookie 为浏览器复制来的字符串
+        :param cookie: 登录过的社区网站 cookie
+        """
+        self.session.headers['Cookie'] = cookies
+
+    def checkin(self):
+        """
+        签到函数
+        """
+        url = 'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin'
+        msg = self.session.get(url)
+        if self.__json_check(msg):
+            return msg.json()
+        return msg.content
+
+if __name__ == '__main__':
+    sb = SMZDM_Bot()
+	# 换成自己的cookie
+    cookies = "Cookie: XXX"
+    sb.load_cookie_str(cookies)
+    res = sb.checkin()
+    print(res)
+    print('签到执行完毕')
